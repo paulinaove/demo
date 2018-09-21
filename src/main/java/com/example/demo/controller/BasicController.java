92d8f88998e4febcb30e3517b6f9cefd933f1e11
paulina.overaite <overaite@gmail.com>
2018-09-21 07:33:36 +0300

    Unneeded imports deleted

--- src/main/java/com/example/demo/controller/BasicController.java
+++ src/main/java/com/example/demo/controller/BasicController.java
@@ -3,10 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.dataStructures.Point;
 import com.example.demo.dataStructures.SquareAndLineReply;
 import com.example.demo.service.InterceptionService;
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
